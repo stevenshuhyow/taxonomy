b1b368605e287f53ee5a826364ace2beb17c1ce6
stevenshuhyow <stevenshuhyow@gmail.com>
2015-03-30 15:14:15 -0700

    Implemented Grub class functionality

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,14 @@
 var Grub = function() {
+//everything here is for all instances
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
 };
+//everything here is registered at the class level
+//and shared between all instances
+Grub.prototype = Object.create(Grub.prototype);
+Grub.prototype.constructor = Grub;
+//class methods
+Grub.prototype.eat = function(){
 
+}
