6ad37c434187bc94e78f1504b0d84c4f2aa5dfae
stevenshuhyow <stevenshuhyow@gmail.com>
2015-03-30 15:38:17 -0700

    Implemented Bee class functionality

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
+//define class
 var Bee = function() {
+  // Call superclass
+  Grub.call(this);
+
+  //define class properties
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+//setup psuedo-classical plumbing
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+// class methods
